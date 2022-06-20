ccb3640c28d008b0bdf03f2ac2268621fae8c493
Neo Temores <neo@Mac.lan>
2022-06-20 16:10:31 -0500

    about page done

--- src/components/aboutpage/About.jsx
+++ src/components/aboutpage/About.jsx
@@ -2,9 +2,18 @@ import React from "react";
 
 const About = () => {
     return (
-        <div>
-            About page
-        </div>
+        <>
+            <h4 className="aboutPgTitle">About this application...</h4>
+            <div className="aboutPgContainer">
+                <p className="indent paragraphTitle">
+                    {'\t'}This application was created by Neo Xavier Temores, in memory of U.S. Army Specialist Hunter Alan Ritter. </p>
+
+                <p className="indent paragraphBody">Hunter, my brother, please forgive me. I am sorry for letting you down, and for not noticing that you were in pain. I am sorry that you felt alone, and for never asking if you were okay.
+                    One day, when we meet again, I hope that you accept my apology. Until then brother, I hope you know that I will continue to share my screen with you.</p>
+
+                <p>Your memory will live on, may you rest in peace.</p>
+            </div>
+        </>
     )
 }
 
